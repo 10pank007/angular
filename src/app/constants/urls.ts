4c9992ee2ccaaf0972544111aff4fd34153c95b1
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-12-06 14:32:53 +0200

    first task

---
+++ src/app/constants/urls.ts
@@ -0,0 +1,17 @@
+const baseURL = 'https://jsonplaceholder.typicode.com'
+
+const users = `${baseURL}/users`;
+const posts = '/posts';
+
+const urls = {
+  users: {
+    base: users,
+    byId:(id: number):string=> `${users}/${id}`
+  },
+  posts: {
+    base: baseURL + posts,
+    postById: (id: number):string => `${users}/${id}${posts}`
+  }
+}
+
+export {urls}
