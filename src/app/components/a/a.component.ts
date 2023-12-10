8f595cf17440c139140f3ac424fde0b9b79be84a
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-12-10 12:24:44 +0200

    lesson3 part1/v1

---
+++ src/app/components/a/a.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {CounterService} from "../../services/counter.service";
+
+@Component({
+  selector: 'app-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './a.component.html',
+  styleUrl: './a.component.css'
+})
+export class AComponent implements OnInit {
+  count: number
+
+  constructor(private counterService: CounterService) {
+  }
+  ngOnInit(): void {
+    this.counterService.getCount().subscribe(value => this.count = value);
+  }
+
+}
