9e0eeab8f8c4d896ae037b67064f89ffc28142ce
Bohdan Pankiv <pankivbogdan96@gmail.com>
2023-12-14 18:18:12 +0200

    almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutsComponent } from './layouts/main-layouts/main-layouts.component';
+import { HeaderComponent } from './layouts/main-layouts/header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MainInterceptor} from "./main.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutsComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: MainInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/layouts/main-layouts/main-layouts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layouts',
+  templateUrl: './main-layouts.component.html',
+  styleUrl: './main-layouts.component.css'
+})
+export class MainLayoutsComponent {
+
+}
